0bd7f3700b0c7d9ec096b6f6ca2aeac3c34b3818
blasterizer <blasterizer007@gmail.com>
2023-09-14 21:22:42 +0100

    Fixed some minor bugs

--- main.py
+++ main.py
@@ -11,22 +11,25 @@ option = st.selectbox("Select data to view",
 st.subheader(f"{option} for the next {days} in {place}")
 
 if place:
-    # Get the temperature and the sky data
-    filtered_data = get_data(place, days)
+    try:
+        # Get the temperature and the sky data
+        filtered_data = get_data(place, days)
 
-    # Create a temperature block
-    if option == "Temperature":
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        figure = express.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperatures (C)"})
-        st.plotly_chart(figure)
+        # Create a temperature block
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = express.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperatures (C)"})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        image_paths = [images[condition] for condition in sky_conditions]
-        print(sky_conditions)
-        st.image(image_paths, width=115)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write("This place does not exist")
 
 
